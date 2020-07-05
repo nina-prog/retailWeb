b8e08bf6c0ae4a3ac8a0c7e9bcd664ce7edebf33
Nina M <65343815+nina-prog@users.noreply.github.com>
2020-07-05 21:12:52 +0200

    initiate springboot project PRIS (Pandemic Retail Information System)

---
+++ PRIS/src/main/java/praktikum/AIFB/RISP/PrisApplication.java
@@ -0,0 +1,13 @@
+package praktikum.AIFB.RISP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrisApplication.class, args);
+	}
+
+}

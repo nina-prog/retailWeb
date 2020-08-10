5c5c62a1eae451cf5e4d26ab929ff5d41e49aa02
Jellenso <jakob.ellensohn@gmail.com>
2020-08-10 15:09:27 +0200

    ProductComponent updatet

--- src/Components/todo/ProductComponent.jsx
+++ src/Components/todo/ProductComponent.jsx
@@ -4,23 +4,40 @@ import Tomatos from '../../img/Tomatos.jpeg'
 class ProductComponent extends Component {
     render(){
         return (
-            <>
-                <h1>Titelxxxx</h1>
-                <img src={Tomatos} width='100' heigt='100' />
-                <div className="container">
-                    Preis
-                    Zur Merkliste
-                    Notify when in Stock
+            <div className="container">
+                <div className="col ">
+                    <div className="row-sm">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-sm">
+                                <img src={Tomatos} width='250' heigt='250' />
+                                </div>
+                                <div className="col-sm align-self-center">
+                                
+                                    <div >Price: </div>
+                                    <p> <button type="button" class="btn btn-primary mu-1">Add to wish list</button></p>
+                                    <p><button type="button" class="btn btn-success">Notify when in Stock</button> </p>
+                                    
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className ="row-sm">
+                        <h1>Product Description</h1>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+                    </div>    
+                    
+                    
                 </div>
-                <div>
-                    <h2> Produktbeschreibung </h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    </p>
-                </div>
-            </>
+            </div>
+          
+                
+            
         )
     }
 }
+
+
 
 export default ProductComponent;

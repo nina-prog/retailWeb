c567eba0411ab65072616f44c1458322177ba4c9
Joel <joel.thalmann@gmail.com>
2020-08-10 10:35:26 +0200

    Header Update WebRetail

--- src/Components/todo/HeaderComponent.jsx
+++ src/Components/todo/HeaderComponent.jsx
@@ -10,7 +10,7 @@ class HeaderComponent extends Component {
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="http://capture-me.io">captureme</a></div>
+                    <div><a href="http://capture-me.io">WebRetail</a></div>
                     <ul className="navbar-nav">
                         {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>}
                         {isUserLoggedIn && <li><Link className="nav-link" to="/todos">Todos</Link></li>}

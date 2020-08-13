04155a306ab2eb69085a974f519eb5cfd433c911
Joel <joel.thalmann@gmail.com>
2020-08-13 16:37:24 +0200

    joel product

--- src/Components/todo/ProductComponent.jsx
+++ src/Components/todo/ProductComponent.jsx
@@ -7,9 +7,26 @@ class ProductComponent extends Component {
     constructor(props) {
         super (props)
         this.state = {
+            productId: 1,
+            category: null,
+            picture: null,
+            name: 'Cucumber',
+            price: null,
+            retailStore: null,
+            description: `This is the description of a cucumber. It is realy tasty and you gonne like it. It's fresh. Come and buy it today!`,
+            limitations: 0.99,
+            remainingStock: null,
+            
+            
+            
             product_id: '1',
             isWishlistOn: true
         }
+        
+        
+
+
+
         this.addToWhishListClicked = this.addToWhishListClicked.bind(this);
         this.deleteFromWhishListClicked = this.deleteFromWhishListClicked.bind(this);
         this.deleteWhishListClicked = this.deleteWhishListClicked.bind(this);
@@ -43,6 +60,9 @@ class ProductComponent extends Component {
 
         deleteWhishListClicked() {
             WishListService.deleteWishList()
+            document.getElementById("title").innerHTML = this.state.name;
+            document.getElementById("price").innerHTML = this.state.price;
+            document.getElementById("description").innerHTML = this.state.description;
         }
 
     render() {
@@ -56,8 +76,8 @@ class ProductComponent extends Component {
                                 <img src={Tomatos} width='250' heigt='250' />
                                 </div>
                                 <div className="col-sm align-self-center">
-                                
-                                    <div >Price: </div>
+                                    <h1><div id="title">Titel</div></h1>
+                                    <div id="price">Price: 5.90</div>
                                     <input type="text" name="product_id" value={this.state.product_id} onChange={this.handleChange}/>
                                     <button className="btn btn-success"onClick={this.addToWhishListClicked}> addToWhishList </button>
                                     <button className="btn btn-success"onClick={this.deleteFromWhishListClicked}> Delete from Whish List </button>
@@ -70,8 +90,8 @@ class ProductComponent extends Component {
                         </div>
                     </div>
                     <div className ="row-sm">
-                        <h1>Product Description</h1>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+                        <h3>Product Description</h3>
+                        <p id="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
                     </div>    
                     
                     
